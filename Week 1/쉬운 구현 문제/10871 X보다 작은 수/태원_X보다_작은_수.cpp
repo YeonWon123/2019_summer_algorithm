94797264bd6460e16ce4f0180bd4275cb159f2c3
ntw408 <ntw408@gmail.com>
2019-06-28 12:50:11 +0900

    Day 1 - 태원 - X보다 작은 수

---
+++ Week 1/쉬운 구현 문제/10871 X보다 작은 수/태원_X보다_작은_수.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	int N, X;
+	cin >> N >> X;
+
+	int a[100];
+	for (int i = 0; i < N; i++) {
+		cin >> a[i];
+	}
+	for (int i = 0; i < N; i++) {
+		if (a[i] < X)
+		{
+			cout << a[i];
+		}
+
+	}
+	return 0;
+}

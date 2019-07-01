aeb7cdc65e26e731229315f0d5b1d6a996086d23
auddnjs <auddnjs2008@naver.com>
2019-07-01 12:50:04 +0900

    명원-숫자의 합

---
+++ Week 1/중간 구현 문제/11720 숫자의 합/명원_숫자의 합.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int N;
+char num;
+
+int main(void)
+{
+	int sum = 0;
+	int temp;
+	cin >> N;
+	cin.get(); //마지막에 "\n" 지워주기 위해  // cin.get()는 문자 하나를 입력받기 위해 사용
+
+	for (int i = 0; i < N; i++) {
+
+		cin.get(num);
+		temp = num - '0';
+		sum += temp;
+	}
+	
+	cout << sum;
+	system("pause");
+	return 0;
+}

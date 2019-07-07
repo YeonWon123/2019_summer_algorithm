d926afdc32fb21437ac2d8cd38b771215a1d9d48
YeonWon123 <46421507+YeonWon123@users.noreply.github.com>
2019-07-07 23:02:06 +0900

    윤원_1주차_정리_및_2주차

---
+++ Week 1/윤원_week1_문제정리/윤원_단어_정렬.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <set>
+#include <string>
+using namespace std;
+
+set <pair<long, string>> s;
+set <pair<long, string>>::iterator iter;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	string str;
+	for (int i = 0; i < n; i++) {
+		cin >> str;
+		s.insert(make_pair(str.length(), str));
+	}
+
+	for (iter = s.begin(); iter != s.end(); iter++) {
+		cout << iter->second << "\n";
+	}
+
+	return 0;
+}

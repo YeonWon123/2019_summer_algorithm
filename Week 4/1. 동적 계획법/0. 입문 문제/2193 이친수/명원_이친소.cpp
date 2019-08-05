6e8b5919478d2fa9096dbde647b9aeefe5c1ded8
auddnjs <auddnjs2008@naver.com>
2019-08-05 15:22:22 +0900

    명원- 이친소

---
+++ Week 4/1. 동적 계획법/0. 입문 문제/2193 이친수/명원_이친소.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int N;
+long long arr[91];
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+
+	cin >> N;
+
+	arr[1] = 1; arr[2] = 1;
+
+	for (int i = 3; i <= N; i++) {
+
+
+		arr[i] = arr[i - 1] + arr[i - 2];
+
+	}
+
+	cout << arr[N];
+
+	//system("pause");
+	return 0;
+}

25b83a5db08488967091eda0e7c35189090cb3de
ntw408 <ntw408@gmail.com>
2019-08-19 13:22:26 +0900

    태원_다각형의면적

---
+++ Week 7/2. 기하 알고리즘 - 벡터의 활용/2166 다각형의 면적/태원_다각형의_면적.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+#define maxValue 10001
+#define lli long long int
+int n;
+lli result;
+pair<lli, lli> p[maxValue], origin;
+
+lli ccw(pair<lli, lli> r, pair<lli, lli> p, pair<lli, lli> q)
+{
+	//lli first = (p.first - r.first) * (q.second - r.second);
+	//lli second = (p.second - r.second) * (q.first - r.first);
+	//lli result = first - second;
+	lli result = (r.first * p.second) - (r.second - p.first) + (p.first * q.second) - (q.first * p.second) + (q.first * r.second) - (r.first * q.second);
+	return result;
+}
+
+int main()
+{
+	cin >> n;
+	for(int i = 1; i <= n; i++)
+	{
+		cin >> p[i].first >> p[i].second;
+	}
+
+	origin = p[1];
+	for (int i = 2; i <= n - 1; i++)
+	{
+		lli ccw_result = ccw(origin, p[i], p[i + 1]);
+		result += ccw_result;
+	}
+	if (result < 0) result *= -1;
+
+	cout << 0.5 * result;
+
+	if (result % 2 == 0)
+		cout << ".0";
+	else
+		cout << ".5";
+	return 0;
+}
